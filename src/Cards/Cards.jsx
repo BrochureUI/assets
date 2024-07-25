63648e0b1db57abad7748566e66af83fea233230
rhman <rhmanibrahimmuhammad@gmail.com>
2024-07-25 21:19:19 +0300

    created JSON file for repositories

--- src/Cards/Cards.jsx
+++ src/Cards/Cards.jsx
@@ -1,126 +1,29 @@
+import repositories from "./reposiotirs.json";
 import "./Cards.css";
 
 
 const Cards = () => {
     return (
         <main id="scheme-cards">
-            <section>
-                <i className="material-symbols-outlined">account_circle</i>
-                <h2>API authenticate</h2>
-                <p>A Python Django/DRF application that handles authentication and account management.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-python"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/api.authen" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">admin_panel_settings</i>
-                <h2>API authorize</h2>
-                <p>A Python Django/DRF application that handles authorization groups and permissions.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-python"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/api.authorization" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">forum</i>
-                <h2>WS chat</h2>
-                <p>A Python Django/DRF and web socket application that handles real time activities (chat and polls).</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-python"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/api.chat" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">responsive_layout</i>
-                <h2>UI blocks</h2>
-                <p>A JavaScript, React (Vite) components library which is used to build a responsive flex layout.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-js"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/blocks" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">input</i>
-                <h2>UI fields</h2>
-                <p>A JavaScript, React (Vite) components library which provides styled form and input components.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-js"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/fields" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">notifications</i>
-                <h2>UI notifications</h2>
-                <p>A JavaScript, React (Vite) components library which provies notifications management.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-js"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/notify" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">account_circle</i>
-                <h2>UI authenticate</h2>
-                <p>A JavaScript React application that views and manages the user authentication data.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-js"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/ui.authen" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">admin_panel_settings</i>
-                <h2>UI authorize</h2>
-                <p>A JavaScript React application that views and manages the user authorization roles, permissions and groups data.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-js"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/api.authorization" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
-            <section>
-                <i className="material-symbols-outlined">forum</i>
-                <h2>UI chat</h2>
-                <p>A JavaScript React application that views and manages real time activities (chat and polls) data.</p>
-                <ul>
-                    <li>
-                        <i className="fa-brands fa-js"></i>
-                    </li>
-                    <li>
-                        <a href="https://github.com/rhman-ibrahim/ui.chat" className="fa-brands fa-github" target="_blank"></a>
-                    </li>
-                </ul>
-            </section>
+            {
+                repositories.map(
+                    (repository, index) => (
+                        <section key={ index }>
+                            <i className={ repository.icon.class }>{ repository.icon.name }</i>
+                            <h2>{ repository.title }</h2>
+                            <p>{ repository.description }</p>
+                            <ul>
+                                <li>
+                                    <i className={`${ repository.icons[0].class } ${ repository.icons[0].name }`}></i>
+                                </li>
+                                <li>
+                                    <a className={`${ repository.icons[1].class } ${ repository.icons[1].name }`} href={ repository.link } target="_blank"></a>
+                                </li>
+                            </ul>
+                        </section>
+                    )
+                )
+            }
         </main>
     )
 }
