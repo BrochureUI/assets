fb38ab786c16fddb9cdcc44fa9a3ffc592eaaea4
rhman <rhmanibrahimmuhammad@gmail.com>
2024-07-25 20:38:11 +0300

    created packages cards

---
+++ src/Cards/Cards.jsx
@@ -0,0 +1,128 @@
+import "./Cards.css";
+
+
+const Cards = () => {
+    return (
+        <main id="scheme-cards">
+            <section>
+                <i className="material-symbols-outlined">account_circle</i>
+                <h2>API authenticate</h2>
+                <p>A Python Django/DRF application that handles authentication and account management.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-python"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">admin_panel_settings</i>
+                <h2>API authorize</h2>
+                <p>A Python Django/DRF application that handles authorization groups and permissions.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-python"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">forum</i>
+                <h2>WS chat</h2>
+                <p>A Python Django/DRF and web socket application that handles real time activities (chat and polls).</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-python"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">responsive_layout</i>
+                <h2>UI blocks</h2>
+                <p>A JavaScript, React (Vite) components library which is used to build a responsive flex layout.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-js"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">input</i>
+                <h2>UI fields</h2>
+                <p>A JavaScript, React (Vite) components library which provides styled form and input components.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-js"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">notifications</i>
+                <h2>UI notifications</h2>
+                <p>A JavaScript, React (Vite) components library which provies notifications management.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-js"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">account_circle</i>
+                <h2>UI authenticate</h2>
+                <p>A JavaScript React application that views and manages the user authentication data.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-js"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">admin_panel_settings</i>
+                <h2>UI authorize</h2>
+                <p>A JavaScript React application that views and manages the user authorization roles, permissions and groups data.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-js"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <i className="material-symbols-outlined">forum</i>
+                <h2>UI chat</h2>
+                <p>A JavaScript React application that views and manages real time activities (chat and polls) data.</p>
+                <ul>
+                    <li>
+                        <i className="fa-brands fa-js"></i>
+                    </li>
+                    <li>
+                        <a className="fa-brands fa-github"></a>
+                    </li>
+                </ul>
+            </section>
+        </main>
+    )
+}
+
+export default Cards;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './header/Header'
+import Cards from './Cards/Cards'
 import "./css/Master.css"
 
 ReactDOM.
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root'))
 .render(
   <React.StrictMode>
     <Header />
+    <Cards />
   </React.StrictMode>,
 )
